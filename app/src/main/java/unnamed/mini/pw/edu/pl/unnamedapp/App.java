b52452b2ec323273ea53f5dadbf5ddf95ee36988
Marcin Chudy <marcinchudy94@gmail.com>
2016-04-13 12:33:04 +0200

    Add main application class and install Timber

---
+++ app/src/main/java/unnamed/mini/pw/edu/pl/unnamedapp/App.java
@@ -0,0 +1,18 @@
+package unnamed.mini.pw.edu.pl.unnamedapp;
+
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if(BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
