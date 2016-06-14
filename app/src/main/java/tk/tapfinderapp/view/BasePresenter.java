560bdda83df289dc9dbb564ad70e33f5cdbb7e63
Marcin Chudy <marcinchudy94@gmail.com>
2016-06-14 20:44:25 +0200

    Introduce presenter interface and base implementation

---
+++ app/src/main/java/tk/tapfinderapp/view/BasePresenter.java
@@ -0,0 +1,16 @@
+package tk.tapfinderapp.view;
+
+public class BasePresenter<T> implements Presenter<T> {
+
+    protected T view;
+
+    @Override
+    public void attachView(T view) {
+        this.view = view;
+    }
+
+    @Override
+    public void detachView() {
+        this.view = null;
+    }
+}
--- app/src/main/java/tk/tapfinderapp/view/beer/BeerDetailsPresenter.java
+++ app/src/main/java/tk/tapfinderapp/view/beer/BeerDetailsPresenter.java
@@ -2,29 +2,39 @@ package tk.tapfinderapp.view.beer;
 
 import javax.inject.Inject;
 
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import timber.log.Timber;
 import tk.tapfinderapp.service.GoogleMapsApiService;
 import tk.tapfinderapp.service.TapFinderApiService;
+import tk.tapfinderapp.view.BasePresenter;
 
-public class BeerDetailsPresenter {
+public class BeerDetailsPresenter extends BasePresenter<BeerDetailsFragment> {
+
+    private Subscription subscription;
+    private TapFinderApiService apiService;
+    private GoogleMapsApiService googleMapsService;
 
     @Inject
-    TapFinderApiService apiService;
-
-    @Inject
-    GoogleMapsApiService googleMapsService;
-
-    @Inject
-    public BeerDetailsPresenter() {
+    public BeerDetailsPresenter(TapFinderApiService apiService, GoogleMapsApiService googleMapsService) {
+        this.apiService = apiService;
+        this.googleMapsService = googleMapsService;
     }
 
     public void loadBeerDetails(int beerId, BeerDetailsFragment fragment) {
-        apiService.getBeerDetails(beerId)
+        subscription = apiService.getBeerDetails(beerId)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(fragment::updateDetails,
                         t -> Timber.wtf(t, "loading beer details"));
+    }
+
+    @Override
+    public void detachView() {
+        super.detachView();
+        if (subscription != null) {
+            subscription.unsubscribe();
+        }
     }
 }
